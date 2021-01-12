77e89541fb0cf58774b75af82e2789be9e718290
Alessandra Berretta <66798751+alessandraberretta@users.noreply.github.com>
2021-01-12 15:44:29 +0100

    Added Condor flag

--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,7 @@ from manipulate_txt import manipulate_txt
 from cosm_calc import cosm_calc
 import subprocess
 from argparse import ArgumentParser
+import os
 
 
 def write_bash_script(file, redshift, dist_lum):
@@ -15,6 +16,28 @@ def write_bash_script(file, redshift, dist_lum):
     subprocess.run("chmod +x script.sh", shell=True, check=True)
 
 
+def write_bash_script_condor(file, redshift, dist_lum, job_dir, fitter_dir):
+
+    with open(job_dir + '/script.sh', 'w') as script:
+        script.write("#!/usr/bin/env bash\n")
+        script.write("source activate JetFit\n")
+        script.write(
+            f"python {fitter_dir}/fitter.py --grb {file} --z {redshift} --dL {dist_lum}")
+    subprocess.run(f"chmod +x {job_dir}/script.sh", shell=True, check=True)
+
+
+def write_file_sub(job_dir):
+
+    with open(job_dir + '/sub', 'w') as script:
+        script.write("universe   = vanilla\n")
+        script.write(f"executable = {job_dir}/script.sh\n")
+        script.write(f"log={job_dir}/output.log\n")
+        script.write(f"output={job_dir}/output.out\n")
+        script.write(f"error={job_dir}/output.error\n")
+        script.write('+ OWNER="AlessandraB"\n')
+        script.write("queue")
+
+
 def main(args=None):
 
     parser = ArgumentParser(description='Pipeline for GRB analysis')
@@ -23,6 +46,10 @@ def main(args=None):
                         dest="grb_name", help="GRB name")
     parser.add_argument("-year", "--year", type=int,
                         dest="grb_year", help="GRB year")
+    parser.add_argument("-condor", "--condor",
+                        dest="condor", action='store_true', help="condor")
+    parser.add_argument("-pathdir", "--pathdir", type=str,
+                        dest="pathdir", help="pathdir")
     args = parser.parse_args()
 
     grb_input, redshift = download_data(year=args.grb_year)
@@ -36,9 +63,25 @@ def main(args=None):
         if grb_output == "":
             continue
 
-        write_bash_script(grb_output, redshift[idx], dist_lum)
+        if args.condor:
 
-        subprocess.run("bash script.sh", shell=True, check=True)
+            job_dir = args.pathdir + '/' + 'job_' + str(idx)
+
+            os.mkdir(job_dir)
+
+            write_bash_script_condor(
+                grb_output, redshift[idx], dist_lum, job_dir, os.getcwd())
+
+            write_file_sub(job_dir)
+
+            # subprocess.run(
+            # f"condor_submit -spool {job_dir}/sub", shell=True, check=True)
+
+        else:
+
+            write_bash_script(grb_output, redshift[idx], dist_lum)
+
+            subprocess.run("bash script.sh", shell=True, check=True)
 
 
 if __name__ == '__main__':
